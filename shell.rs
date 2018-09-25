931c956599536b4fefff246273cfb77d52f42872
Gaurav Desale <desalegaurav60@gmail.com>
2018-09-25 13:06:01 -0500

    adding wrong stuff

--- shell.rs
+++ shell.rs
@@ -2,4 +2,15 @@ fn main() {
 	lsh_loop();
 }
 fn lsh_loop {
-	
+	let x = &line as *const i32;
+	let args = os::args_as_bytes();
+	let status : i32;
+	while(status) {
+		println!("> ");
+		x = lsh_read_line();
+		args = lsh_split_line(x);
+		status = lsh_execute(args);
+		free(line);
+		free(args);
+	}
+}	
